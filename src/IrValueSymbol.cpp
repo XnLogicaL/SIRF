be6dc1a210b1ee1e95f5cdb4d70369ec431cc686
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-05-22 00:54:39 +0300

    Implement IrValueSymbol

---
+++ src/IrValueSymbol.cpp
@@ -0,0 +1,16 @@
+// This file is a part of the SIRF (Simple Intermediate Representation Format)
+// project Copyright (C) 2025 XnLogical - Licensed under GNU GPL v3.0
+
+#include <SIRF/IR/IrValueSymbol.hpp>
+
+namespace SIRF {
+
+constexpr bool IrValueSymbol::isLvalue() const {
+  return false;
+}
+
+std::string IrValueSymbol::toString() const {
+  return '@' + id;
+}
+
+} // namespace SIRF
