36efcae9a2fc357fce0f5e829e9a42a74ebd6971
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-05-24 00:43:09 +0300

    Implement IrStmtAssign

---
+++ src/IrStmtAssign.cpp
@@ -0,0 +1,12 @@
+// This file is a part of the SIRF (Simple Intermediate Representation Format)
+// project Copyright (C) 2025 XnLogical - Licensed under GNU GPL v3.0
+
+#include <IR/IrStmtAssign.hpp>
+
+namespace SIRF {
+
+std::string IrStmtAssign::toString() const {
+  return lvalue->toString() + " = " + rvalue->toString() + '\n';
+}
+
+} // namespace SIRF
